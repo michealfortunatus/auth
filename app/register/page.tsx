c08602eb7b080158f3c7a8d7239202ed3417891c
forrune <esoifeoma1@gmail.com>
2025-11-24 14:53:40 +0100

    Update Auth

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,7 +3,7 @@ import AuthForm from "@/components/AuthForm";
 export default function RegisterPage() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
-      <AuthForm submitLabel="Create Account" />
+      <AuthForm submitLabel="Sign Up" />
     </div>
   );
 }
