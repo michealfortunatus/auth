d244d0fc974e25472fb06b7cfb3ca9451f7114ba
forrune <esoifeoma1@gmail.com>
2025-11-25 09:06:54 +0100

    Update Config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+    theme: {extend: {}},
+  plugins: [],
+
+}
+
